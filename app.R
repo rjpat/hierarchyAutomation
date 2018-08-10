3ec20bcd84e42d4653e6f21547e1e043106cb7e6
Raymond Patterson <rj.patterson95@gmail.com>
2018-08-10 14:57:59 +1000

    Added minty theme, currently not implemented, and working through building the shiny app structure

--- app.R
+++ app.R
@@ -1,7 +1,29 @@
 library(shiny)
 
-ui <- fluidPage(
+ui <- fluidPage(#theme = 'mintyTheme.css',
+   titlePanel('Area Hierarchy Creation'),
    
+   sidebarLayout(
+     sidebarPanel(
+       fileInput('baseData', 'Input Area Hierarchy .csv',
+                 multiple = FALSE,
+                 accept = c("text/csv",
+                            "text/comma-separated-values,text/plain",
+                            ".csv")),
+       
+       numericInput('numTiers', 'Number of Tiers', 5, min = 1, max = 6, step = 1)
+     
+      
+      ),
+     
+     mainPanel(
+       tabsetPanel(
+         tabPanel('Input'),
+         
+         tabPanel('Output')
+       )
+     )
+   )
 )
 
 
