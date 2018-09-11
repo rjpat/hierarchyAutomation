e037e740936b2593f1b284a9567d6ef1bd908b13
Raymond Patterson <rj.patterson95@gmail.com>
2018-09-11 15:31:50 +1000

    Added the ability to change the ouput between the processed hierarchy and the marked up original dataset. Also added the ability to download the outcome of the above.

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ columnNameInput <- function(inputNumber, inputType) {
   )
 }
 
+
+
 ui <- fluidPage(#theme = 'mintyTheme.css',
   titlePanel('Area Hierarchy Creation'),
   
@@ -21,10 +23,21 @@ ui <- fluidPage(#theme = 'mintyTheme.css',
                    ".csv")
       ),
       
+      # column(6, radioButtons('outRadio', label = h6('Which Output'),
+      #                     choices = list('Altered' = 1, 'Raw' = 2),
+      #                     selected = 1,
+      #                     inline = TRUE)),
+      # column(6, downloadButton('downloadOut', 'Download Output')),
+
       sliderInput('numTiers', 'Number of Tiers', 1, min = 1, max = 6, step = 1),
 
       
       column(6,
+        # radioButtons('outRadio', label = h6('Which Output'),
+        #              choices = list('Altered' = 1, 'Raw' = 2), 
+        #              selected = 1,
+        #              inline = TRUE),
+             
         columnNameInput(1, 'Name'),
         
         columnNameInput(2, 'Name'),
@@ -39,6 +52,8 @@ ui <- fluidPage(#theme = 'mintyTheme.css',
       ),
       
       column(6,
+        # downloadButton('downloadOut', 'Download Output'),
+        
         columnNameInput(1, 'ShortName'),
         
         columnNameInput(2, 'ShortName'),
@@ -60,6 +75,14 @@ ui <- fluidPage(#theme = 'mintyTheme.css',
         ),
                           
         tabPanel('Output',
+          column(12,
+            column(6, radioButtons('outRadio', label = h6('Which Output'),
+                       choices = list('Altered' = 1, 'Raw' = 2),
+                       selected = 1,
+                       inline = TRUE)),
+            column(6, downloadButton('downloadOut', 'Download Output'))
+          ),
+                 
           tableOutput('finalTable')
                  )
       )
@@ -81,83 +104,83 @@ server <- function(input, output) {
   
 
   
-  final <- reactive({
+  processed <- reactive({
     loadFrame <- setNames(data.frame(matrix(ncol = 4, nrow = 0)), c('name', 'shortName', 'parent', 'futureId'))
     
     data <- rawSourceData() %>%
       mutate(parent0 = 'a')
     
+    
     tierLoops <- function(tierName, tierShort, tierNum) {
-      
-      #'Intermediate produced here to remove any issues of shortname not being assigned. 
+
+      #'Intermediate produced here to remove any issues of shortname not being assigned.
       #'If there is no designated shortname then the name is duplicated in the main table and assigned as short.
       #'The reason for duplication is to prevent a select issue where you cannot select the same column multiple times
       tierShortInt <- if(tierShort == 0) {
         data <- data %>%
           bind_cols(data[tierName])
-        
+
         tierShort <- ncol(data)
       } else {
         tierShort
       }
-      
+
       prevDataParent <- paste0('parent', tierNum-1)
-      
+
       cleansed <- data %>%
         select(tierName, tierShort, prevDataParent) %>%
         unique() %>%
-        rename('name' = !!names(.[1]), 
-               'shortName' = !!names(.[2]), 
+        rename('name' = !!names(.[1]),
+               'shortName' = !!names(.[2]),
                'parent' = !!names(.[3])) %>%
         #Removes any rows with an na name or parent (where previous tier had an na name)
         filter(!is.na(name)) %>%
         filter(!is.na(parent)) %>%
         #Arranges the data in the correct order then adds the additional background columns required for producing the hierarchy
         arrange(parent, name, shortName) %>%
-        mutate(shortName = trimws(substring(shortName, 1, 25)), 
-               id = row_number(), 
-               parentNum = 0, 
-               futureId = 0) 
-      
+        mutate(shortName = trimws(substring(shortName, 1, 25)),
+               id = row_number(),
+               parentNum = 0,
+               futureId = 0)
+
       prevParent <- 0
-      
+
       #Produces the area hierarchy details through the loop
       for(i in cleansed$id) {
         currParent <- cleansed[i, 3]
-        
+
         if(currParent == prevParent) {
           parentNum = parentNum + 1
         } else {
-          parentNum = 1 
+          parentNum = 1
         }
-        
+
         cleansed[i, 5] <- parentNum
-        
+
         cleansed[i, 6] <- paste0(currParent, '.', parentNum)
-        
+
         prevParent = currParent
       }
-      
+
       #removes non-necessary columns
       cleansedPrep <<- cleansed %>%
         select(name, shortName, parent, futureId)
-      
+
       #Creates the loading dataframe to be utilised at the end
       loadFrame <<- rbind(loadFrame, cleansedPrep)
-      
+
       #Prep and join the data back to the original dataset allowing for the function to be called again
       cleansedPrep2 <- cleansedPrep %>%
-        select(name, parent, futureId) 
-      
+        select(name, parent, futureId)
+
       named <- colnames(data[tierName])
       named2 <- colnames(data[prevDataParent])
-      
+
       data <<- data %>%
         left_join(cleansedPrep2, by = c(setNames('name', named), setNames('parent', named2))) %>%
         rename(!!paste0('parent', tierNum) := futureId)
-    
-      return(loadFrame)
     }
+
     
     for(i in 1:(max(input$numTiers))) {
       a <- eval(parse(text = (paste0('input$column', 'Name', i))))
@@ -167,7 +190,11 @@ server <- function(input, output) {
       tierLoops(a, b, i)
     }
     
-    loadFrame
+    if(input$outRadio != 1) {
+      data
+    } else {
+      loadFrame
+    }
   })
 
   
@@ -177,8 +204,16 @@ server <- function(input, output) {
   })
   
   output$finalTable <- renderTable({
-    final()
+    processed()
   })
+
+  output$downloadOut <- downloadHandler(
+    filename = function(){'AreaHierarchy.csv'},
+    content = function(holder){
+      write.csv(processed(), holder)
+    }
+  )
+  
 }
 
 shinyApp(ui = ui, server = server)
